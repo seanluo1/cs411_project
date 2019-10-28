ece1980268c0c7cb2c370938a83506d5e314c11d
melodyfelix <harmonyfelix@gmail.com>
2019-10-27 22:33:16 -0500

    deleted duplicate files

--- README.md
+++
@@ -1,6 +0,0 @@
-# cs411_project
-
-make install: to install project dependencies
-
-make run: to run flask server
-
--- views/routes.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template, Flask, Blueprint
-
-index_blueprint = Blueprint("index", __name__ ,template_folder='templates')
-
-@index_blueprint.route("/")
-def test1():
-    return render_template("hello.html")
-
