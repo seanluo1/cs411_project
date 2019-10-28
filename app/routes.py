ded7528d8efdec192d60dd9dcdf178a29e2977bf
nmodak2 <nmodak2@illinois.edu>
2019-10-27 23:11:26 -0500

    Added basic db functionality

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,30 @@
-from flask import render_template, Flask, Blueprint
-
+import functools
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from . import db
 index_blueprint = Blueprint("index", __name__ ,template_folder='templates')
 
 @index_blueprint.route("/")
 def test1():
+    db_instance = db.get_db()
+    db_instance.execute(
+        'INSERT INTO User(Id, FirstName, LastName, Email, Password) VALUES (?, ?, ?, ?, ?)',
+        (1, "Bob", "Smith", "bob@smith.com", "password")
+    )
+    db_instance.commit()
+
     return render_template("hello.html")
+
+
+@index_blueprint.route("/woo")
+def test2():
+    db_instance = db.get_db()
+    db_instance.execute(
+        'SELECT * FROM User'
+    )
+    
+
+    return render_template("hello.html")
+
 
