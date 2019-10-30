3b1c3e955d5496b6d13ce3b5a1c123e6a912153e
melodyfelix <harmonyfelix@gmail.com>
2019-10-30 13:19:06 -0500

    add route to check tables

--- app/routes.py
+++ app/routes.py
@@ -5,25 +5,38 @@ from flask import (
 from . import db
 index_blueprint = Blueprint("index", __name__ ,template_folder='templates')
 
-@index_blueprint.route("/")
+@index_blueprint.route("/add_user", methods=['POST'])
 def test1():
+    data = request.get_json()
+    first_name = data["first_name"] 
+    last_name = data["last_name"]
+    email = data["email"]
+    password = data["password"]
+
     db_instance = db.get_db()
     db_instance.execute(
-        'INSERT INTO User(Id, FirstName, LastName, Email, Password) VALUES (?, ?, ?, ?, ?)',
-        (1, "Bob", "Smith", "bob@smith.com", "password")
+        'INSERT INTO User(FirstName, LastName, Email, Password) VALUES (?, ?, ?, ?)',
+        (first_name, last_name, email, password)
     )
     db_instance.commit()
 
     return render_template("hello.html")
 
 
-@index_blueprint.route("/woo")
+@index_blueprint.route("/check_users_db")
 def test2():
     db_instance = db.get_db()
-    db_instance.execute(
-        'SELECT * FROM User'
-    )
-    
+    user_table = db_instance.execute("SELECT * FROM User")
+    all_users = {}
+
+    for item in user_table.fetchall():
+        all_users["user_id"] = item[0]
+        all_users["first_name"] = item[1]
+        all_users["last_name"] = item[2]
+        all_users["email"] = item[3]
+        all_users["password"] = item[4]
+
+    print(all_users)
 
     return render_template("hello.html")
 
