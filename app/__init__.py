3a0e598a102e2de12ba2d4d39f03574c4387fbdf
nmodak2 <nmodak2@illinois.edu>
2019-11-30 21:27:57 -0600

    added flask-bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 import os
 
 from flask import Flask
+from flask_bootstrap import Bootstrap
 
 
 
@@ -11,6 +12,8 @@ def create_app(test_config=None):
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'app.sqlite'),
     )
+
+    Bootstrap(app)
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
