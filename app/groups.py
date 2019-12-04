f9ebbba69324e5e7a6014c4fc62dcbe24eda4e4d
nmodak2 <nmodak2@illinois.edu>
2019-12-03 21:55:39 -0600

    Stuff

--- app/groups.py
+++ app/groups.py
@@ -25,10 +25,11 @@ def all_groups():
     my_groups = list(mycol.find({ "admin_id": current_user_id }))
 
 
-    return render_template("nav_bar/my_groups.html", data =my_groups )
+    return render_template("groups/my_groups.html", data =my_groups )
 
 
 @bp.route('/new_group', methods=('GET', 'POST'))
+@login_required
 def new_group():
     current_user_id = session.get('user_id')
 
@@ -51,12 +52,86 @@ def new_group():
 
         if error:
             flash(error, 'danger')
-            return render_template('nav_bar/add_group.html')
+            return render_template('groups/add_group.html')
 
 
-        new_group = {"id": 1, "name": group_name, "admin_id": current_user_id}
+        new_group = {"id": mycol.count() + 1, "name": group_name, "admin_id": current_user_id, "members": [current_user_id]}
         mycol.insert_one(new_group)
 
         return redirect(url_for('groups.all_groups'))
 
-    return render_template('nav_bar/add_group.html')
+    return render_template('groups/add_group.html')
+
+
+@bp.route("/groups/id/<int:group_id>")
+@login_required
+def group_page(group_id):
+    current_user_id = session.get('user_id')
+    db_instance = get_db()
+    myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+    mydb = myclient["mydatabase"]
+    mycol = mydb["groups"]
+    error = None
+
+    group = list(mycol.find({ "id": group_id }))[0]
+    group_member_ids = group['members']
+
+    if current_user_id not in group_member_ids:
+        flash(error, 'danger')
+        return redirect(url_for('home.index'))
+    
+    print(group_member_ids)
+    group_members_table = db_instance.execute("SELECT * FROM User WHERE id IN (%s)" % ','.join('?'*len(group_member_ids)), group_member_ids).fetchall()
+    print(group_members_table)
+    
+    print(group)
+    group_members = []
+
+    
+
+    for member in group_members_table:
+        member_dict = {}
+        member_dict["id"] = member[0]
+        member_dict["name"] = member[1] + " " + member[2]
+        member_dict["sotw"] = member[5]
+        group_members.append(member_dict)
+
+    print(group_members)
+
+    return render_template("groups/group_page.html", data=(group, group_members)  )
+
+
+
+
+@bp.route("/groups/id/<int:group_id>/add_member", methods=['GET', 'POST'])
+@login_required
+def add_member(group_id):
+    db_instance = get_db()
+    myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+    mydb = myclient["mydatabase"]
+    mycol = mydb["groups"]
+    error = None
+
+    group = list(mycol.find({ "id": group_id }))[0]
+    print(group)
+
+    all_results = {}
+
+    if request.method == 'POST':
+
+        query = request.form['searchName']
+        formatted_query = "\"%" + query + "%\""
+        sql_query = "SELECT * FROM User WHERE FirstName || ' ' || LastName LIKE " + formatted_query
+        print(sql_query)
+        query_results = db_instance.execute(sql_query)
+        all_results = []
+        for item in query_results.fetchall():
+            user_dict = {}
+            user_dict["id"] = item[0]
+            user_dict["name"] = item[1] + ' ' + item[2]
+            all_results.append(user_dict)
+        return render_template("groups/add_member.html", data=(group, all_results)  )
+
+
+
+    return render_template("groups/add_member.html", data=(group, [])  )
